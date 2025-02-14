4c151a1e3228225da23963dd577af69355be2580
PetarKazandzhiev <petar.kazandzhiev@gmail.com>
2025-02-14 16:49:49 +0200

    updated the cypress.yml file again pt 23

--- cypress/e2e/catalog_order_workflow.cy.js
+++ cypress/e2e/catalog_order_workflow.cy.js
@@ -2,7 +2,7 @@ describe('Catalog and Order Workflow Test', () => {
   it('navigating through the catalog, interacting with the Amazon brand, adding items to the cart, and completing the order.', () => {
     // Set up intercepts BEFORE calling cy.login
     cy.intercept('POST', 'https://proxy.duegate.com/staging/oauth/token', {
-      statusCode: 200,
+      //statusCode: 200,
       body: {
         access_token: 'dummy-token',
         refresh_token: 'dummy-refresh-token',
